d1b05c926a80e197bda2d234d6a0aefe20bf30a3
Mukam7 <mukamhojaniyazow89@gmail.com>
2023-04-09 18:25:39 +0500

    first commit

--- main.js
+++
@@ -1,6 +0,0 @@
-var menuButton = document.getElementById("menu-button");
-var menu = document.getElementById("menu");
-menuButton.addEventListener("click", function () {
-  menu.classList.toggle("show");
-});
-
